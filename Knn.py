b5a2b224a9c6b8d6757904d8bd0af34f64b54b68
settawut1410 <eisrun0001@gmail.com>
2025-04-09 11:56:20 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -1,7 +1,8 @@
 from sklearn.neighbors import KNeighborsClassifier
-import streamlit as st
+import streamlit as st 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 st.title('การจำแนกข้อมูลด้วยเทคนิค ML')
+st.image("./img/moew.jpg")
