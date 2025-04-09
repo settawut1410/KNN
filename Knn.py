77ee44831fa9f2c11ac791afb63e3e2d6a12d05c
settawut1410 <eisrun0001@gmail.com>
2025-04-09 12:08:35 +0700

    .

--- Knn.py
+++ Knn.py
@@ -10,19 +10,19 @@ st.image("./img/moew.jpg")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-   st.header("Versicolor")
-   st.image("./img/iris1.jpg")
+   st.header("spiderman")
+   st.image("./img/s1.jpg")
 
 with col2:
-   st.header("Verginiga")
-   st.image("./img/iris2.jpg")
+   st.header("spiderman")
+   st.image("./img/s2.jpg")
 
 with col3:
-   st.header("Setosa")
-   st.image("./img/iris3.jpg")
+   st.header("spiderman")
+   st.image("./img/s3.jpg")
 
 html_7 = """
-<div style="background-color:#c5f18a;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:#808080;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>ข้อมูล iris หรือข้อมูลดอกไม้สำหรับทำนาย</h5></center>
 </div>
 """
